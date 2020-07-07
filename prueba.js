4403c5508d1dd29be6f063f4141d84f741971e3e
KAlanizK <karinaalaniz1994@gmail.com>
2020-07-07 12:11:10 -0300

    fundamentos JavaScript 30

--- prueba.js
+++ prueba.js
@@ -56,10 +56,102 @@ meses.splice(2, 1);
   //Revertir
   meses.reverse();
   //Arreglo
+
    let arreglo1 = [1,2,3],
        arreglo2 = [9,8,7];
-console.log(arreglo1.concat(arreglo2));
+//console.log(arreglo1.concat(arreglo2));
+
+//Ordenar un arreglo con letras alfabeticamente
+
+const frutas = ['Banana', 'Manzana', 'Naranja', 'Fresas', 'Zanahoria'];
+
+frutas.sort();
+//console.log(frutas);
+
+//Ordenar numeros x - y (menos a mayor)/ y - x (mayor a menor)
+
+arreglo1 = [3,9,10,78,100,54,89,5,2,46,35];
+ arreglo1.sort(function(x, y){
+     return x- y;
+ })
+  //console.log(arreglo1);
 
 //Encontrar un elemento en un arreglo
 //console.log(meses);
 
+//Crear un objeto(para llamar a uno poner "punto" y el nombre)
+const persona = {
+      nombre :'Karina',
+      apellido : 'Alaniz Kogan',
+      profesion : 'Programadora',
+      email : 'karina.alaniz.kogan@gmail.com',
+
+      //Agregar arreglo y acceder 
+      edad : 20,
+      musica : ['Queen', 'Mercedes Sosa', 'Silvio Rodriguez'],
+      hogar: {
+          ciudad: 'CABA',
+          pais: 'Argentina'
+      },
+      nacimiento: function() {
+          return new Date().getFullYear() - this.edad;
+      }
+}
+//persona.musica.push('Alternativo');
+//console.log(persona.nacimiento() );
+
+//Como crear un arreglo de objetos 
+//Mezcla entre definir un arreglo y agregarle objetos 
+
+const autos = [ 
+    {modelo: 'Citroen', motor: 6.2},
+    {modelo: 'Fiat', motor: 6.1},
+    {modelo: 'Hyundai', motor: 6.3}
+ ];
+  
+for(let i = 0; i <autos.length; i++ ) {
+    //console.log(`${autos[i].modelo} ${autos[i].motor}`);
+}
+//Con string literal, $, {} y ``. colocando los datos a gusto, y los valores accediendo a ellos de forma individual si se pueden modificar.
+autos[0].modelo = 'Audi';
+//console.log(autos);
+
+//Function Declaration
+ function saludar(nombre){
+     //console.log(`Hola ${nombre}`);
+ }
+  saludar ('Ruben');
+  saludar ('Karina');
+  saludar ('Veronica');
+
+  //Suma
+   function sumar (a,b){
+      //console.log(a+b);
+  }
+    sumar(1,2);
+    sumar(3+4);
+function sumar(a,b){
+    return a +b;
+}
+let suma;
+suma = sumar (1,2);
+suma = sumar (3,4);
+suma = sumar (5,2);
+
+//console.log(suma);
+
+function saludar(nombre = 'Visitante') {
+    return `hola ${nombre}`;
+}
+ let saludo;
+ saludo = saludar();
+
+ //console.log(saludo);
+
+//Function expression
+
+const suma2 = function(a =3, b =5){
+    return a + b;
+}
+ console.log( suma2(1,2) );
+ console.log(suma2 (5));
